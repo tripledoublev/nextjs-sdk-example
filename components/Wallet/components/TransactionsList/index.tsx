66739538684f3ab3ebc4a583d25069ee57c6c715
Yoav Assulin <yoav@paytweed.com>
2023-01-29 17:10:42 +0200

    improve code block

--- components/Wallet/components/TransactionsList/index.tsx
+++ components/Wallet/components/TransactionsList/index.tsx
@@ -1,5 +1,5 @@
 import { SubTitle, Table, Td, Th } from "@/public/style";
-
+const chains = ["tezos", "polygon", "ethereum"];
 interface CryptoCurrencyToken {
   contractAddress: string;
   decimals: number;
@@ -26,96 +26,42 @@ interface Data {
   tezos: any[];
 }
 
-const TransactionsList: React.FC<{ data: Data }> = ({ data }) => {
+const TransactionsList: React.FC<{ data: Data; chain: string }> = ({
+  data,
+}) => {
   return (
     <>
-      <Table>
-        <thead>
-          <SubTitle>Ethereum</SubTitle>
-          <tr>
-            <Th>Created At</Th>
-            <Th>Crypto Currency Amount</Th>
-            <Th>Token Name</Th>
-            <Th>Token Symbol</Th>
-            <Th>Direction</Th>
-            <Th>From Address</Th>
-            <Th>To Address</Th>
-            <Th>Status</Th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.ethereum.map((entry) => (
-            <tr key={entry.id}>
-              <Td>{entry.createdAt}</Td>
-              <Td>{entry.cryptoCurrencyAmount}</Td>
-              <Td>{entry.cryptoCurrencyToken.name}</Td>
-              <Td>{entry.cryptoCurrencyToken.symbol}</Td>
-              <Td>{entry.direction}</Td>
-              <Td>{entry.fromAddress}</Td>
-              <Td>{entry.toAddress}</Td>
-              <Td>{entry.status}</Td>
+      {chains.map((chain) => (
+        <Table>
+          <thead>
+            <SubTitle>{chain}</SubTitle>
+            <tr>
+              <Th>Created At</Th>
+              <Th>Crypto Currency Amount</Th>
+              <Th>Token Name</Th>
+              <Th>Token Symbol</Th>
+              <Th>Direction</Th>
+              <Th>From Address</Th>
+              <Th>To Address</Th>
+              <Th>Status</Th>
             </tr>
-          ))}
-        </tbody>
-      </Table>
-      <Table>
-        <thead>
-          <SubTitle>Tezos</SubTitle>
-          <tr>
-            <Th>Created At</Th>
-            <Th>Crypto Currency Amount</Th>
-            <Th>Token Name</Th>
-            <Th>Token Symbol</Th>
-            <Th>Direction</Th>
-            <Th>From Address</Th>
-            <Th>To Address</Th>
-            <Th>Status</Th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.tezos.map((entry) => (
-            <tr key={entry.id}>
-              <Td>{entry.createdAt}</Td>
-              <Td>{entry.cryptoCurrencyAmount}</Td>
-              <Td>{entry.cryptoCurrencyToken.name}</Td>
-              <Td>{entry.cryptoCurrencyToken.symbol}</Td>
-              <Td>{entry.direction}</Td>
-              <Td>{entry.fromAddress}</Td>
-              <Td>{entry.toAddress}</Td>
-              <Td>{entry.status}</Td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
-      <Table>
-        <thead>
-          <SubTitle>Polygon</SubTitle>
-          <tr>
-            <Th>Created At</Th>
-            <Th>Crypto Currency Amount</Th>
-            <Th>Token Name</Th>
-            <Th>Token Symbol</Th>
-            <Th>Direction</Th>
-            <Th>From Address</Th>
-            <Th>To Address</Th>
-            <Th>Status</Th>
-          </tr>
-        </thead>
-        <tbody>
-          {data.polygon.map((entry) => (
-            <tr key={entry.id}>
-              <Td>{entry.createdAt}</Td>
-              <Td>{entry.cryptoCurrencyAmount}</Td>
-              <Td>{entry.cryptoCurrencyToken.name}</Td>
-              <Td>{entry.cryptoCurrencyToken.symbol}</Td>
-              <Td>{entry.direction}</Td>
-              <Td>{entry.fromAddress}</Td>
-              <Td>{entry.toAddress}</Td>
-              <Td>{entry.status}</Td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {data[chain].map((entry) => (
+              <tr key={entry.id}>
+                <Td>{entry.createdAt}</Td>
+                <Td>{entry.cryptoCurrencyAmount}</Td>
+                <Td>{entry.cryptoCurrencyToken.name}</Td>
+                <Td>{entry.cryptoCurrencyToken.symbol}</Td>
+                <Td>{entry.direction}</Td>
+                <Td>{entry.fromAddress}</Td>
+                <Td>{entry.toAddress}</Td>
+                <Td>{entry.status}</Td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      ))}
     </>
   );
 };
