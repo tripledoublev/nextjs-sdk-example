befd82de0c3802eecedc097aac748225cb5d6540
tripledoublev <vincentcharlebois@gmail.com>
2023-11-08 19:26:47 +0100

    Refactor: Isolate Default Blockchain IDs to Constants
    - Moved defaultBlockchainIds to a dedicated constants file to decouple client-side and server-side code dependencies.
    - Updated imports in _app.tsx and tweed.service.ts to reference the new constants file.
    - This change ensures that client-side components do not indirectly import server-side modules like 'fs', resolving the build errors.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import { TweedFrontendSdkProvider } from "@paytweed/frontend-sdk-react";
 import type { AppProps } from "next/app";
 import { useCallback } from "react";
-import { defaultBlockchainIds } from '../services/tweed.service';
+import { defaultBlockchainIds } from '../services/constants';
 
 export default function App({ Component, pageProps }: AppProps) {
   const sendMessageToBackend = useCallback(async (message: string) => {
--- services/tweed.service.ts
+++ services/tweed.service.ts
@@ -1,20 +1,20 @@
 import { TweedBackendSDK } from "@paytweed/backend-sdk";
 import nftService from "./nft.service";
+import { defaultBlockchainIds } from './constants';
 
 type AsyncReturnType<T> = T extends (...args: any[]) => Promise<infer R>
   ? R
   : never;
 
-export const defaultBlockchainIds = ["tezosGhost"];
 
 class TweedService {
   private _client: AsyncReturnType<typeof TweedBackendSDK.setup> | undefined =
     undefined;
   async initialize() {
     this._client = await TweedBackendSDK.setup({
-      apiKey: "YOUR-API-KEY",
-      apiSecret: "YOUR-API-SECRET",
-      defaultBlockchainIds: ["tezosGhost"],
+      apiKey: "WUjkeKW5uNZV54q7LKxqGJF5Dl8BbTYp",
+      apiSecret: "60YzL7LJs5NbWQyy-xaRAndOQ7Xhd0mM4sztdSN1p6AA7Db2y3IIL22lMrSmXfnd",
+      defaultBlockchainIds: defaultBlockchainIds,
       callbacks: {
         getNftPurchaseData: async ({ nftId }) => nftService.getById(nftId),
       },
